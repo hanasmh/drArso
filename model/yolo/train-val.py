9346cb1aa94ef2fe7f97ea930012634663fddd7a
hanas.hanis <hanasmuchammad@gmail.com>
2024-03-22 06:46:36 +0700

    ngedit

--- model/yolo/train-val.py
+++ model/yolo/train-val.py
@@ -4,15 +4,15 @@ def train_yolo(model_type, data_path, epochs):
     
     # Choose the appropriate YOLO model based on the user's input
     if model_type == 'yolov8n':
-        model = YOLO('/content/BRIN/model/yolo/version/yolov8/yolov8n.pt')
+        model = YOLO('/content/BRIN/model/yolo/yolov8n.pt')
     elif model_type == 'yolov8s':
-        model = YOLO('/content/BRIN/model/yolo/version/yolov8/yolov8s.pt')
+        model = YOLO('/content/BRIN/model/yolo/yolov8s.pt')
     elif model_type == 'yolov8m':
-        model = YOLO('/content/BRIN/model/yolo/version/yolov8/yolov8m.pt')
+        model = YOLO('/content/BRIN/model/yolo/yolov8m.pt')
     elif model_type == 'yolov8l':
-        model = YOLO('/content/BRIN/model/yolo/version/yolov8/yolov8l.pt')
+        model = YOLO('/content/BRIN/model/yolo/yolov8l.pt')
     elif model_type == 'yolov8x':
-        model = YOLO('/content/BRIN/model/yolo/version/yolov8/yolov8x.pt')
+        model = YOLO('/content/BRIN/model/yolo/yolov8x.pt')
     else:
         raise ValueError("Invalid model type. Supported types: yolov8n, yolov8s, yolov8m, yolov8l, yolov8x")
 
